3ca4288199a682b45c5e918fc50228c4a3602262
Koukaakiva <koukaakiva@gmail.com>
2022-11-16 12:30:36 -0500

    Initial Commit

---
+++ src/main/java/com/revature/CharacterTracker/CharacterTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.revature.CharacterTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharacterTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharacterTrackerApplication.class, args);
+	}
+
+}
