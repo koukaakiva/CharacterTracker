a11027ee3cd2e016f1d40cb873ff23eeeb0fb022
Koukaakiva <koukaakiva@gmail.com>
2022-11-17 14:25:58 -0500

    Home page

--- frontend/src/Components/Home.tsx
+++ frontend/src/Components/Home.tsx
@@ -1,10 +1,56 @@
+import { useNavigate } from "react-router-dom";
 import HomeHeader from "./HomeHeader";
 
 export default function Home() {
+  const navigate = useNavigate();
+
+  function navigateTo(destination: string) {
+    navigate("/" + destination);
+    window.location.reload();
+  }
+
   return (
     <>
       <HomeHeader />
-      plop
+      <div
+        id="templatemo_content_right"
+        style={{
+          width: "100%",
+          height: "100%",
+          margin: "0",
+          flexFlow: "column",
+          display: "flex",
+        }}
+      >
+        <table style={{ textAlign: "center" }}>
+          <td>
+            <a onClick={() => navigateTo("Novels")}>
+              <div
+                id="image-circle"
+                style={{
+                  display: "inline-block",
+                  backgroundImage:
+                    'url("//cdn.pixabay.com/photo/2015/11/19/21/11/atlas-1052011_1280.jpg")',
+                }}
+              />
+              <h2>Novels</h2>
+            </a>
+          </td>
+          <td>
+            <a onClick={() => navigateTo("Characters")}>
+              <div
+                id="image-circle"
+                style={{
+                  display: "inline-block",
+                  backgroundImage:
+                    'url("//cdn.pixabay.com/photo/2016/12/04/02/01/celtic-woman-1880944_1280.jpg")',
+                }}
+              />
+              <h2>Characters</h2>
+            </a>
+          </td>
+        </table>
+      </div>
     </>
   );
 }
--- frontend/src/Components/HomeHeader.tsx
+++ frontend/src/Components/HomeHeader.tsx
@@ -3,23 +3,20 @@ export default function HomeHeader() {
     <div id="templatemo_header">
       <div id="templatemo_title">
         <h1>Character Tracker</h1>
+        <br />
       </div>
 
       <div id="templatemo_special_offers">
-        <p>
-          <span>25%</span> discounts for purchase over $80
-        </p>
+        Feature Coming Soon...
+        <br />
         <a href="subpage.html" style={{ marginLeft: "50px" }}>
           Read more...
         </a>
       </div>
 
       <div id="templatemo_new_books">
-        <ul>
-          <li>Suspen disse</li>
-          <li>Maece nas metus</li>
-          <li>In sed risus ac feli</li>
-        </ul>
+        Feature Coming Soon...
+        <br />
         <a href="subpage.html" style={{ marginLeft: "50px" }}>
           Read more...
         </a>
